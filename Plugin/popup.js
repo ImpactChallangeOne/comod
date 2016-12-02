ef67b66292a28c88dd7512717222f03126d10434
ClevelandBraun <lukasjulian.braunschweig@googlemail.com>
2016-12-02 14:56:37 +0100

    plugin

---
+++ Plugin/popup.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $('#ex1').slider({
+      formatter: function(value) {
+        return 'Current value: ' + value;
+      }
+    });
+});
--- javascript.js
+++ javascript.js
@@ -18,8 +18,6 @@ $(document).ready(function(){
                 
         inputtext.documents[0].text = $("#input").val();
 
-        console.log(inputtext[0]["text"])
-
         console.log(JSON.stringify(inputtext));
 
         $.ajax({
@@ -28,10 +26,9 @@ $(document).ready(function(){
                headers: {"Ocp-Apim-Subscription-Key": "7eaf0a6c25f449a29581c1a86a536057"},
                contentType: "application/json",
                dataType: "json",
-               data: inputtext.documents[0].text
+               data: JSON.stringify(inputtext)
                })
                .done(function( msg ) {
-                    console.log(msg);
                     $("#response").text("The sentiment score is: " + msg.documents[0].score);
                 });
             });
